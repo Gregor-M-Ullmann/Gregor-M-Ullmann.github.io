6171d03b2ce96848a6912f233598eb7296caa7fb
Gregor-M-Ullmann <g.mayerullmann@gmail.com>
2015-05-11 17:56:41 +0200

    login1

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
 $(function(){
 
-	$('#submit').on('click',function(){
-		$('body').toggleClass("black");
+	var username = localStorage.getItem("username");
+	var password = localStorage.getItem("password");
+
+	var $username = $('#username');
+	var $password = $('#password');
+	var $bottles = $('#bottles');
+	var $submit = $('#submit');
+	var $logout = $('#logout');
+
+	var activeProfile;
+
+	$username.val(username);
+	$password.val(password);
+
+	logIn();
+
+
+
+	$submit.on('click',function(){
+		logIn();
+	})
+
+	$logout.on('click',function(){
+		$('#loginPage').fadeIn();
+		localStorage.setItem("username", '');
+		localStorage.setItem("password", '');
 	})
 
 
+
+	function logIn(){
+		try{
+			for(var i=0; i<=profiles.length; i++){
+				if($username.val()===''||$password.val()===''){
+					break;
+				}
+				if($username.val()===profiles[i].name&&$password.val()===profiles[i].password){
+					localStorage.setItem("username", $username.val());
+		        	localStorage.setItem("password", $password.val());
+		           	activeProfile=profiles[i];
+		        	$('#loginPage').fadeOut();
+					$username.val('');
+					$password.val('');
+					break;
+				}
+			}
+		}
+		catch(err){
+			$username.val('');
+			$password.val('');
+			alert("Falscher Name oder Passwort");
+		}	
+	}
 });
---
+++ profiles.js
@@ -0,0 +1,43 @@
+var admin={
+	name:"admin",
+	password:"password",
+	bottles:0
+};
+
+var gregor={
+	name:"gregor",
+	password:"password",
+	bottles:0
+};
+
+var martin={
+	name:"martin",
+	password:"password",
+	bottles:0
+};
+
+var roxana={
+	name:"roxana",
+	password:"password",
+	bottles:0
+};
+
+var niklas={
+	name:"niklas",
+	password:"password",
+	bottles:0
+};
+
+var katharina={
+	name:"katharina",
+	password:"password",
+	bottles:0
+};
+
+var marinus={
+	name:"marinus",
+	password:"password",
+	bottles:0
+};
+
+var profiles = [admin,gregor,martin,roxana,niklas,katharina,marinus];
